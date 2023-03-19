1937a001956879dfb0b773919e7ad85e572238ab
Vishruti Patil <vishruti2908@gmail.com>
2023-03-20 00:15:14 +0530

    allowing user to select X or 0

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class TicTacToeGame {
     private char[] board = new char[10];
+    private char playerLetter;
+    private char computerLetter;
 
     public TicTacToeGame() {
         for (int i = 1; i < board.length; i++) {
@@ -15,9 +18,25 @@ public class TicTacToeGame {
         System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9] + " ");
     }
 
-    public static void main(String[] args) {
-        TicTacToeGame game = new TicTacToeGame();
-        game.displayBoard();
+    public void chooseLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose your letter: X or O");
+        playerLetter = scanner.nextLine().toUpperCase().charAt(0);
+        while (playerLetter != 'X' && playerLetter != 'O') {
+            System.out.println("Invalid input. Choose your letter: X or O");
+            playerLetter = scanner.nextLine().toUpperCase().charAt(0);
+        }
+        if (playerLetter == 'X') {
+            computerLetter = 'O';
+        } else {
+            computerLetter = 'X';
+        }
+        System.out.println("You chose " + playerLetter + ". Computer will be " + computerLetter + ".");
     }
 
+    public static void main(String[] args) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.chooseLetter();
+        game.displayBoard();
+    }
 }
