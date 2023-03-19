7f84d5bb1485eb3aacef61f625b59d797156bb65
Vishruti Patil <vishruti2908@gmail.com>
2023-03-20 01:05:46 +0530

    Checking if opponent can win then make a move to block it

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,109 +1,107 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToeGame {
-    private char[] board = new char[10];
-    private char playerLetter;
-    private char computerLetter;
-    private boolean playerTurn;
+    static char[] board = new char[10];
+    static char playerLetter;
+    static char computerLetter;
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
 
-    public TicTacToeGame() {
+    public static void createBoard() {
+        Arrays.fill(board, ' ');
+    }
+
+    public static void chooseLetter() {
+        while (true) {
+            System.out.print("Choose a letter 'X' or 'O': ");
+            char letter = Character.toUpperCase(scanner.next().charAt(0));
+            if (letter == 'X' || letter == 'O') {
+                playerLetter = letter;
+                computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+                break;
+            } else {
+                System.out.println("Invalid choice. Please choose 'X' or 'O'.");
+            }
+        }
+    }
+
+    public static void showBoard() {
+        System.out.println("\nBoard:");
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+
+    public static void makeMove(char letter, int position) {
+        board[position] = letter;
+    }
+
+    public static boolean isSpaceFree(int position) {
+        return board[position] == ' ';
+    }
+
+    public static int getUserMove() {
+        while (true) {
+            System.out.print("Enter a position (1-9) to make a move: ");
+            int move = scanner.nextInt();
+            if (move >= 1 && move <= 9 && isSpaceFree(move)) {
+                return move;
+            } else {
+                System.out.println("Invalid move. Please choose an empty cell between 1-9.");
+            }
+        }
+    }
+
+    public static int getComputerMove() {
         for (int i = 1; i < board.length; i++) {
-            board[i] = ' ';
-        }
-        playerTurn = false;
-    }
-
-    public void displayBoard() {
-        System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3] + " ");
-        System.out.println("---+---+---");
-        System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6] + " ");
-        System.out.println("---+---+---");
-        System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9] + " ");
-    }
-
-    public void showBoard() {
-        System.out.println("Current board:");
-        displayBoard();
-        System.out.println();
-    }
-
-    public void chooseLetter() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose your letter: X or O");
-        playerLetter = scanner.nextLine().toUpperCase().charAt(0);
-        while (playerLetter != 'X' && playerLetter != 'O') {
-            System.out.println("Invalid input. Choose your letter: X or O");
-            playerLetter = scanner.nextLine().toUpperCase().charAt(0);
-        }
-        if (playerLetter == 'X') {
-            computerLetter = 'O';
-        } else {
-            computerLetter = 'X';
-        }
-        System.out.println("You chose " + playerLetter + ". Computer will be " + computerLetter + ".");
-    }
-
-    public boolean makeMove(int index, char letter) {
-        if (index < 1 || index > 9) {
-            System.out.println("Invalid input. Choose a number from 1 to 9.");
-            return false;
-        }
-        if (board[index] != ' ') {
-            System.out.println("That cell is already occupied. Choose a different cell.");
-            return false;
-        }
-        board[index] = letter;
-        return true;
-    }
-
-    public void tossCoin() {
-        Random random = new Random();
-        int result = random.nextInt(2);
-        if (result == 0) {
-            System.out.println("Heads! You play first.");
-            playerTurn = true;
-        } else {
-            System.out.println("Tails! Computer plays first.");
-        }
-    }
-    public void playComputerTurn() {
-        System.out.println("Computer's turn");
-        int computerMove = -1;
-
-        // Check if computer can win in the next move
-        for (int i = 1; i <= 9; i++) {
-            if (board[i] == ' ') {
-                board[i] = computerLetter;
-                if (checkForWin(computerLetter)) {
-                    computerMove = i;
-                    break;
+            if (isSpaceFree(i)) {
+                makeMove(computerLetter, i);
+                if (checkWinner(board, computerLetter)) {
+                    board[i] = ' ';
+                    return i;
                 }
                 board[i] = ' ';
             }
         }
 
-        // If computer cannot win, make a random move
-        if (computerMove == -1) {
-            computerMove = getRandomMove();
+        for (int i = 1; i < board.length; i++) {
+            if (isSpaceFree(i)) {
+                makeMove(playerLetter, i);
+                if (checkWinner(board, playerLetter)) {
+                    board[i] = ' ';
+                    return i;
+                }
+                board[i] = ' ';
+            }
         }
 
-        board[computerMove] = computerLetter;
-        currentTurn = playerLetter;
-        showBoard();
-    }
+        int[] corners = {1, 3, 7, 9};
+        ArrayList<Integer> possibleMoves = new ArrayList<>();
+        for (int i : corners) {
+            if (isSpaceFree(i)) {
+                possibleMoves.add(i);
+            }
+        }
+        if (!possibleMoves.isEmpty()) {
+            return possibleMoves.get(random.nextInt(possibleMoves.size()));
+        }
 
+        if (isSpaceFree(5)) {
+            return 5;
+        }
 
-    public boolean checkWinner(char letter) {
-        // Check rows
-        if (board[1] == letter && board[2] == letter && board[3] == letter) return true;
-        if (board[4] == letter && board[5] == letter && board[6] == letter) return true;
-        if (board[7] == letter && board[8] == letter && board[9] == letter) return true;
-        // Check columns
-        if (board[1] == letter && board[4] == letter && board[7] == letter) return true;
-        if (board[2] == letter && board[5] == letter && board[8] == letter) return true;
-        if (board[3] == letter && board[6] == letter && board[9] == letter) return true;
-        // Check diagonals
+        int[] sides = {2, 4, 6, 8};
+        possibleMoves.clear();
+        for (int i : sides) {
+            if (isSpaceFree(i)) {
+                possibleMoves.add(i);
+            }
+        }
+        if (!possibleMoves.isEmpty()) {
+            return possibleMoves.get(random.nextInt(possibleMoves.size()));
+        }
+
     }
 }
-
